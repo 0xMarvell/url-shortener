185c2296ec7aa770acb27d17c5b5f03970b7b020
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-12 19:17:57 +0100

    created MapHandler function in handler.go file, fixed import error in main.go file

--- main/main.go
+++ main/main.go
@@ -1,17 +1,25 @@
 package main
 
 /* Exercise details
-The goal of this exercise is to create an http.Handler that will look at the path of any incoming web request and determine if it should redirect the user to a new page, much like URL shortener would.
+The goal of this exercise is to create an http.Handler that will look at the path of any incoming web
+request and determine if it should redirect the user to a new page, much like URL shortener would.
 
-For instance, if we have a redirect setup for /dogs to https://www.somesite.com/a-story-about-dogs we would look for any incoming web requests with the path /dogs and redirect them.
+For instance, if we have a redirect setup for /dogs to https://www.somesite.com/a-story-about-dogs we
+would look for any incoming web requests with the path /dogs and redirect them.
 
-To complete this exercises you will need to implement the stubbed out methods in handler.go. There are a good bit of comments explaining what each method should do, and there is also a main/main.go source file that uses the package to help you test your code and get an idea of what your program should be doing.
+To complete this exercises you will need to implement the stubbed out methods in handler.go. There are
+a good bit of comments explaining what each method should do, and there is also a main/main.go source
+file that uses the package to help you test your code and get an idea of what your program should be
+doing.
 
-I suggest first commenting out all of the code in main.go related to the YAMLHandler function and focusing on implementing the MapHandler function first.
+I suggest first commenting out all of the code in main.go related to the YAMLHandler function and
+focusing on implementing the MapHandler function first.
 
-Once you have that working, focus on parsing the YAML using the gopkg.in/yaml.v2 package. Note: You will need to go get this package if you don't have it already.
+Once you have that working, focus on parsing the YAML using the gopkg.in/yaml.v2 package. Note: You
+will need to go get this package if you don't have it already.
 
-After you get the YAML parsing down, try to convert the data into a map and then use the MapHandler to finish the YAMLHandler implementation. Eg you might end up with some code like this:
+After you get the YAML parsing down, try to convert the data into a map and then use the MapHandler to
+finish the YAMLHandler implementation. Eg you might end up with some code like this:
 
 func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
   parsedYaml, err := parseYAML(yaml)
@@ -26,14 +34,51 @@ But in order for this to work you will need to create functions like parseYAML a
 Bonus
 As a bonus exercises you can also...
 
-Update the main/main.go source file to accept a YAML file as a flag and then load the YAML from a file rather than from a string.
+Update the main/main.go source file to accept a YAML file as a flag and then load the YAML from a file
+rather than from a string.
 Build a JSONHandler that serves the same purpose, but reads from JSON data.
-Build a Handler that doesn't read from a map but instead reads from a database. Whether you use BoltDB, SQL, or something else is entirely up to you. */
+Build a Handler that doesn't read from a map but instead reads from a database. Whether you use
+BoltDB, SQL, or something else is entirely up to you. */
 
 import (
 	"fmt"
+	"net/http"
+
+	urlshort "github.com/url-shortener"
 )
 
 func main() {
-	fmt.Println("Hello :)")
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	// 	yaml := `
+	// - path: /urlshort
+	//   url: https://github.com/gophercises/urlshort
+	// - path: /urlshort-final
+	//   url: https://github.com/gophercises/urlshort/tree/solution
+	// `
+	// 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// 	if err != nil {
+	// 		panic(err)
+	// }
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
 }
