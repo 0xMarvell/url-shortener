1035101a31a5dda19984b4db1d5c9eecc610aaf9
Marvellous Chimaraoke <rokemarvellous@gmail.com>
2022-01-14 19:08:10 +0100

    Completed YAMLHandler function, built other smaller functions for better efficiency

--- handler.go
+++ handler.go
@@ -2,7 +2,14 @@ package urlshort
 
 import (
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -37,7 +44,33 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
+
+func parseYAML(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
-	return nil, nil
+	//1. Parse the YAML
+	pathUrls, err := parseYAML(yml)
+	if err != nil {
+		return nil, err
+	}
+	//2. Convert YAML array into map
+	pathsToUrls := buildMap(pathUrls)
+	//3. return a map handler using the map
+	return MapHandler(pathsToUrls, fallback), nil
 }
